0e2cc06f1128fc8cbf30d7b060cd76e18553b040
Alexey Gaponchuk <lapitusik@gmail.com>
2022-09-09 23:02:41 +0300

    home fix

--- qa/src/test/kotlin/ru/sber/qa/ScannerTest.kt
+++ qa/src/test/kotlin/ru/sber/qa/ScannerTest.kt
@@ -2,13 +2,14 @@ package ru.sber.qa
 
 import io.mockk.every
 import io.mockk.mockkObject
-import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Test
+
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
 import kotlin.random.Random
 
 @DisplayName("Scanner class test cases")
 internal class ScannerTest {
-
     private val expectedDataSize = 100
 
 
@@ -48,3 +49,4 @@ internal class ScannerTest {
     }
 
 }
+
