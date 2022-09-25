b3f58ffe03319f8e1c50303d1389b0e6c0c662b2
Vadim Bandel <vadim@bandel.ru>
2022-09-25 23:47:33 +0300

    задание ооп

--- oop/src/main/kotlin/ru/sber/oop/DataClasses.kt
+++ oop/src/main/kotlin/ru/sber/oop/DataClasses.kt
@@ -1,11 +1,18 @@
 package ru.sber.oop
 
-data class User(val name: String, val age: Long) {
-    lateinit var city: String
-}
+data class User(
+    val name: String,
+    val age: Long,
+    var city: String? = null
+)
 
 fun main() {
     val user1 = User("Alex", 13)
-    //TODO: user2 = ...
-    //TODO: user3 = ...
+    val user2 = user1.copy(name = "Petr")
+
+    user1.city = "Omsk"
+
+    val user3 = user1.copy().apply { city = "Tomsk" }
+
+    println(user1 == user3)
 }
---
+++ oop/src/main/kotlin/ru/sber/oop/extensions/MonsterExtensions.kt
@@ -0,0 +1,5 @@
+package ru.sber.oop.extensions
+
+import ru.sber.oop.Monster
+
+fun Monster.getSalutation() = "Hello, I'm $name"
