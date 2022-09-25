394418991f745160f5df4f422995943ec4a85597
Сынков Алексей Васильевич <Synkov.A.Vas@sberbank.ru>
2022-09-25 20:13:28 +0300

    io/nio

--- io/src/main/kotlin/ru/sber/io/Archivator.kt
+++ io/src/main/kotlin/ru/sber/io/Archivator.kt
@@ -1,5 +1,11 @@
 package ru.sber.io
 
+import java.io.File
+import java.io.FileInputStream
+import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
+import java.util.zip.ZipOutputStream
+
 /**
  * Реализовать методы архивации и разархивации файла.
  * Для реализиации использовать ZipInputStream и ZipOutputStream.
@@ -11,6 +17,15 @@ class Archivator {
      * Архив должен располагаться в том же каталоге, что и исходной файл.
      */
     fun zipLogfile() {
+        val sourceFile = File("io/logfile.log")
+        val zipFile = File("io/logfile.zip")
+
+        ZipOutputStream(zipFile.outputStream()).use { zipOutputStream ->
+            sourceFile.inputStream().use { fileInputStream ->
+                zipOutputStream.putNextEntry(ZipEntry(sourceFile.name))
+                zipOutputStream.write(fileInputStream.readAllBytes())
+            }
+        }
 
     }
 
@@ -19,6 +34,14 @@ class Archivator {
      * Извлечь из архива logfile.zip файл и сохарнить его в том же каталоге с именем unzippedLogfile.log
      */
     fun unzipLogfile() {
+        val sourceFile = File("io/unzippedLogfile.log")
+        val zipFile = File("io/logfile.zip")
 
+        ZipInputStream(FileInputStream(zipFile)).use { zipInputStream ->
+            zipInputStream.nextEntry
+            sourceFile.outputStream().use {
+                it.write(zipInputStream.readAllBytes())
+            }
+        }
     }
 }
--- io/src/main/kotlin/ru/sber/nio/Grep.kt
+++ io/src/main/kotlin/ru/sber/nio/Grep.kt
@@ -1,5 +1,16 @@
 package ru.sber.nio
 
+import java.io.File
+import java.io.FileWriter
+import java.nio.file.Files
+import java.nio.file.Files.find
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.nio.file.Paths.*
+import kotlin.io.path.Path
+import kotlin.io.path.isDirectory
+import kotlin.io.path.useLines
+
 /**
  * Реализовать простой аналог утилиты grep с использованием калссов из пакета java.nio.
  */
@@ -14,7 +25,32 @@ class Grep {
      * Пример для подстроки "22/Jan/2001:14:27:46":
      * 22-01-2001-1.log : 3 : 192.168.1.1 - - [22/Jan/2001:14:27:46 +0000] "POST /files HTTP/1.1" 200 - "-"
      */
+
+
     fun find(subString: String) {
 
+        val path = get("io/logs")
+        val resultFile = Paths.get("io/result.txt")
+
+        Files.newOutputStream(resultFile).use { writer ->
+            Files.walk(path).filter { p -> !p.isDirectory() }.forEach { file ->
+                var count = 0;
+                file.useLines { line ->
+                    line.forEach { string ->
+                        count++
+                        if (string.contains(subString)) {
+                            writer.write("${file.fileName} : $count : $string\n".toByteArray())
+                        }
+                    }
+                }
+                count = 0
+            }
+            writer.flush()
+        }
     }
 }
+
+fun main() {
+    Grep().find("54")
+}
+
