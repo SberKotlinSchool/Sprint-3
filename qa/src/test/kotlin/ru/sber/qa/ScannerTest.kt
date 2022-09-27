4d90aebd6d886b7cc297446e183d67e48a6a826b
Gaponchuk Alexey <aggaponchuk@gmail.com>
2022-09-27 11:30:52 +0300

    move mockRandom to @beforeEach on ScannerTest.kt

--- qa/src/test/kotlin/ru/sber/qa/ScannerTest.kt
+++ qa/src/test/kotlin/ru/sber/qa/ScannerTest.kt
@@ -2,22 +2,26 @@ package ru.sber.qa
 
 import io.mockk.every
 import io.mockk.mockkObject
+import io.mockk.unmockkAll
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import kotlin.random.Random
 
 @DisplayName("Scanner class test cases")
 internal class ScannerTest {
-    private val expectedDataSize = 100
+
+    @BeforeEach
+    fun mockRandom() {
+        mockkObject(Random)
+    }
 
     @Test
     fun `getScanData getScanData() test throw ScanTimeoutException with mock`() {
-
-        mockkObject(Random)
         every { Random.nextLong(5000L, 15000L) } returns 11000L
-
         assertThrows(ScanTimeoutException::class.java) {
             Scanner.getScanData()
         }
@@ -25,12 +29,16 @@ internal class ScannerTest {
 
     @Test
     fun `getScanData getScanData() test not throw ScanTimeoutException with mock`() {
-
-        mockkObject(Random)
+        val expectedDataSize = 100
         every { Random.nextLong(5000L, 15000L) } returns 9000L
         val resultScan = Scanner.getScanData()
         assertNotNull(resultScan, "result of getScanData is not null check")
         assertEquals(expectedDataSize, resultScan.size, "data size check")
+    }
+
+    @AfterEach
+    fun unlockAllRandom() {
+        unmockkAll()
     }
 }
 
