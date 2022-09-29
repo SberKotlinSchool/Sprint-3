11b2ad9095955c9aad771f9dba1a7fd331ec1a15
Даниил Тетерин <theweaklessone@yandex.ru>
2022-09-29 16:45:04 +0300

    OOP part

--- oop/src/main/kotlin/ru/sber/oop/DataClasses.kt
+++ oop/src/main/kotlin/ru/sber/oop/DataClasses.kt
@@ -2,10 +2,20 @@ package ru.sber.oop
 
 data class User(val name: String, val age: Long) {
     lateinit var city: String
+
+    override fun equals(other: Any?): Boolean = when (other) {
+        is User -> super.equals(other) && this.city.equals(other.city)
+        else -> super.equals(other)
+    }
 }
 
 fun main() {
     val user1 = User("Alex", 13)
-    //TODO: user2 = ...
-    //TODO: user3 = ...
+    val user2 = user1.copy(name = "Alex2")
+
+    user1.city = "Omsk"
+
+    val user3 = user1.copy().also { it.city = "Tomsk" }
+
+    println(user1.equals(user3))
 }
--- oop/src/main/kotlin/ru/sber/oop/Polymorphism.kt
+++ oop/src/main/kotlin/ru/sber/oop/Polymorphism.kt
@@ -1,13 +1,58 @@
 package ru.sber.oop
 
+import kotlin.math.min
+import kotlin.random.Random
+
 interface Fightable {
     val powerType: String
     var healthPoints: Int
     val damageRoll: Int
+        get() = Random.nextInt()
 
     fun attack(opponent: Fightable): Int
 }
 
-//TODO: create class Player, Monster, Goblin here...
+class Player(
+    val name: String,
+    private val isBlessed: Boolean,
+    override val powerType: String,
+    override var healthPoints: Int
+) : Fightable {
 
+    override fun attack(opponent: Fightable): Int {
+        val damage = damageRoll * if (isBlessed) 2 else 1
 
+        opponent.healthPoints - damage
+
+        // Мы же не можем нанести больше урона, чем есть здоровья у врага
+        return min(opponent.healthPoints, damage)
+    }
+}
+
+abstract class Monster(
+    val name: String,
+    val description: String
+): Fightable {
+
+    override fun attack(opponent: Fightable): Int {
+        opponent.healthPoints - damageRoll
+        return min(opponent.healthPoints, damageRoll)
+    }
+}
+
+class Goblin(
+    override val powerType: String,
+    override var healthPoints: Int,
+): Monster("Goblin", "little green brat") {
+
+    override val damageRoll: Int
+        get() = super.damageRoll / 2
+}
+
+val Room.enemy: Goblin
+    get() = Goblin(
+        powerType = "Nasty scream",
+        healthPoints = Random.nextInt(10, 20)
+    )
+
+fun Monster.getSalutation() = "$name is waving to you"
