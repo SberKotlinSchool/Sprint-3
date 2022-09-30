56e42e3e91ef7754ff40e42dcf9fa21eadf51ec8
Даниил Тетерин <theweaklessone@yandex.ru>
2022-09-30 14:35:24 +0300

    Change OOP problem solving approach

--- oop/src/main/kotlin/ru/sber/oop/DataClasses.kt
+++ oop/src/main/kotlin/ru/sber/oop/DataClasses.kt
@@ -1,28 +1,6 @@
 package ru.sber.oop
 
-data class User(val name: String, val age: Long) {
-    lateinit var city: String
-
-    override fun equals(other: Any?): Boolean = when (other) {
-        is User ->
-            (name == other.name && age == other.age)
-            .takeIf { it && this::city.isInitialized == other::city.isInitialized }
-            .takeIf {
-                it == true &&
-                    if (this::city.isInitialized)
-                        city == other.city
-                    else
-                        true
-            }
-            ?: false
-
-        else -> super.equals(other)
-    }
-
-    override fun hashCode(): Int {
-        return super.hashCode() + city.hashCode()
-    }
-}
+data class User(val name: String, val age: Long, var city: String? = null)
 
 fun main() {
     val user1 = User("Alex", 13)
