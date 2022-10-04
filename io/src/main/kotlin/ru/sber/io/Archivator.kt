8a2e3b2495a235b22468f9b2422dd7dafdbd9ec5
Шамрай Кирилл Константинович <Shamray.K.Ko@sberbank.ru>
2022-10-04 20:30:03 +0300

    Второй спринт. io

--- io/src/main/kotlin/ru/sber/io/Archivator.kt
+++ io/src/main/kotlin/ru/sber/io/Archivator.kt
@@ -1,5 +1,10 @@
 package ru.sber.io
 
+import java.io.File
+import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
+import java.util.zip.ZipOutputStream
+
 /**
  * Реализовать методы архивации и разархивации файла.
  * Для реализиации использовать ZipInputStream и ZipOutputStream.
@@ -11,7 +16,14 @@ class Archivator {
      * Архив должен располагаться в том же каталоге, что и исходной файл.
      */
     fun zipLogfile() {
-
+        try {
+            ZipOutputStream(File("io/logfile.zip").outputStream()).use { outputStream ->
+                File("io/logfile.log").inputStream().use { inputStream ->
+                    outputStream.putNextEntry(ZipEntry("logfile.log"))
+                    outputStream.write(inputStream.readAllBytes())
+                }
+            }
+        } catch (e: Throwable) { }
     }
 
     /**
@@ -19,6 +31,14 @@ class Archivator {
      * Извлечь из архива logfile.zip файл и сохарнить его в том же каталоге с именем unzippedLogfile.log
      */
     fun unzipLogfile() {
-
+        try {
+            ZipInputStream(File("io/logfile.zip").inputStream()).use { inputStream ->
+                inputStream.nextEntry.let {
+                    File("io/unziplogfile.log").outputStream().use { outputStream ->
+                        outputStream.write(inputStream.readAllBytes())
+                    }
+                }
+            }
+        } catch (e: Throwable) { }
     }
 }
