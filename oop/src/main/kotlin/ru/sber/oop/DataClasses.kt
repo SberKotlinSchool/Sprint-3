e8c910637e767d1022526ed8e6ac5870d8f9ca99
romankulinic <kulinichrs@gmail.com>
2022-10-04 12:44:04 +0300

    Oop Done

--- oop/src/main/kotlin/ru/sber/oop/DataClasses.kt
+++ oop/src/main/kotlin/ru/sber/oop/DataClasses.kt
@@ -2,10 +2,35 @@ package ru.sber.oop
 
 data class User(val name: String, val age: Long) {
     lateinit var city: String
+
+    override fun equals(other: Any?): Boolean {
+        return if (other is User) {
+            (this.city ==  other.city)
+                    && (this.age == other.age)
+                    && (this.name == other.name)
+        } else {
+            false
+        }
+    }
+
+    override fun hashCode(): Int {
+        var result = name.hashCode()
+        result = 31 * result + age.hashCode()
+        result = 31 * result + city.hashCode()
+        return result
+    }
+
 }
 
 fun main() {
     val user1 = User("Alex", 13)
-    //TODO: user2 = ...
-    //TODO: user3 = ...
+    val user2 = user1.copy(name = "Gustav")
+    user1.city = "Omsk"
+    val user3 = user1.copy()
+    user3.city = "Tomsk"
+
+    if(user1 == user3) {
+        println("Equals")
+    }
+
 }
--- oop/src/main/kotlin/ru/sber/oop/Inheritance.kt
+++ oop/src/main/kotlin/ru/sber/oop/Inheritance.kt
@@ -1,13 +1,21 @@
 package ru.sber.oop
 
-class Room(val name: String, val size: Int) {
+open class Room(val name: String, val size: Int) {
+    // 2: Добавил доступ толко наслоедникам через protected
+    constructor(_name: String) : this(_name, 100)
+    protected open val dangerLevel = 5
 
-    val dangerLevel = 5
+    open fun description() = "Room: $name"
 
-    fun description() = "Room: $name"
-
-    fun load() = "Nothing much to see here..."
+    internal open fun load() = "Nothing much to see here..."
 
 }
+class TownSquare: Room ( "Town Square" ,1000) {
+    override val dangerLevel = super.dangerLevel - 3
 
-//TODO: create class TownSquare here...
+    // 1: Переопределил load()  и в качестве придумывания строку переопределил ещё и description
+    override fun description() = "Town Square: $name"
+    final override fun load(): String  = "This is a ${this.description()} "
+}
+
+
