c37e5e3043ed6cbcdc5ff02b2bdd4e7f2fe23d76
Georgy K <vangog-2005@ngs.ru>
2022-10-07 21:18:46 +0300

    Sprint-2: reworked HrDepartmentTest

--- qa/src/test/kotlin/ru/sber/qa/HrDepartmentTest.kt
+++ qa/src/test/kotlin/ru/sber/qa/HrDepartmentTest.kt
@@ -1,29 +1,51 @@
 package ru.sber.qa
 
 import io.mockk.every
-import io.mockk.mockk
 import io.mockk.spyk
+import io.mockk.unmockkAll
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
-import java.time.DayOfWeek
-import java.time.LocalDateTime
+import kotlin.math.abs
 import kotlin.random.Random
+import kotlin.test.assertFailsWith
 
 internal class HrDepartmentTest {
 
-    val random = Random(13)
-    var hrDepartment = spyk<HrDepartment>()
-    val localDateTime = mockk<LocalDateTime>()
+    private val employeeId = abs(Random.nextLong())
+    private lateinit var certificateRequest: CertificateRequest
 
-    @Test
-    fun receiveRequest() {
-        every { localDateTime.dayOfWeek } returnsMany listOf(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY, DayOfWeek.WEDNESDAY)
-        assertThrows<WeekendDayException> { hrDepartment.receiveRequest(CertificateRequest(random.nextLong(), CertificateType.NDFL)) }
-        assertThrows<WeekendDayException> { hrDepartment.receiveRequest(CertificateRequest(random.nextLong(), CertificateType.NDFL)) }
-        assertThrows<WeekendDayException> { hrDepartment.receiveRequest(CertificateRequest(random.nextLong(), CertificateType.NDFL)) }
+    @BeforeEach
+    fun init() {
+        certificateRequest = spyk(CertificateRequest(employeeId, CertificateType.NDFL))
+    }
+
+    @AfterEach
+    fun tearDown() {
+        unmockkAll()
     }
 
     @Test
-    fun processNextRequest() {
+    fun receiveRequestTest() {
+
+    }
+
+    @Test
+    fun processNextRequestTest() {
+
+        val hrDepartment = HrDepartment
+        val data = Random.nextBytes(100)
+        val certificate = Certificate(certificateRequest, employeeId, data)
+
+        assertFailsWith<NullPointerException> { hrDepartment.processNextRequest(employeeId) }
+
+        every { certificateRequest.process(employeeId) } returns certificate
+
+        hrDepartment.receiveRequest(certificateRequest)
+        hrDepartment.processNextRequest(employeeId)
+
+//        assert(hrDepartment.outcomeOutcome.size == 1)
+//        assert(hrDepartment.incomeBox.size == 0)
+
     }
 }
