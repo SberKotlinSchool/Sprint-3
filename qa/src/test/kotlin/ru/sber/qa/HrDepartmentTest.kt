ce6098c40a652d70b45933788ebb0d097e87b3b1
Georgy K <vangog-2005@ngs.ru>
2022-10-09 13:07:47 +0300

    Sprint-2: processNextRequestTest ready

--- qa/src/test/kotlin/ru/sber/qa/HrDepartmentTest.kt
+++ qa/src/test/kotlin/ru/sber/qa/HrDepartmentTest.kt
@@ -6,8 +6,13 @@ import io.mockk.unmockkAll
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import java.lang.reflect.Field
+import java.time.Clock
+import java.time.Instant
+import java.time.ZoneOffset
 import kotlin.math.abs
 import kotlin.random.Random
+import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
 
 internal class HrDepartmentTest {
@@ -37,6 +42,8 @@ internal class HrDepartmentTest {
         val data = Random.nextBytes(100)
         val certificate = Certificate(certificateRequest, employeeId, data)
 
+        hrDepartment.clock = Clock.fixed(Instant.parse("2022-10-10T00:00:00Z"), ZoneOffset.UTC)
+
         assertFailsWith<NullPointerException> { hrDepartment.processNextRequest(employeeId) }
 
         every { certificateRequest.process(employeeId) } returns certificate
@@ -44,8 +51,19 @@ internal class HrDepartmentTest {
         hrDepartment.receiveRequest(certificateRequest)
         hrDepartment.processNextRequest(employeeId)
 
-//        assert(hrDepartment.outcomeOutcome.size == 1)
-//        assert(hrDepartment.incomeBox.size == 0)
+        val incomeBox = getHiddenList<CertificateRequest>(hrDepartment, "incomeBox")
+        val outComeBox = getHiddenList<Certificate>(hrDepartment, "outcomeOutcome")
+        val processedCertificate = outComeBox[0]
 
+        assert(incomeBox.isEmpty())
+        assert(outComeBox.size == 1)
+        assertEquals(certificate, processedCertificate)
+
+    }
+
+    private fun <T> getHiddenList(source: HrDepartment, fieldName: String): List<T> {
+        val field: Field = HrDepartment.javaClass.getDeclaredField(fieldName)
+        field.isAccessible = true
+        return field.get(source) as List<T>
     }
 }
