64da518b70540214a5ae70ef4964f212071d0c3d
Georgy K <vangog-2005@ngs.ru>
2022-10-10 21:32:59 +0300

    Sprint-2: oop: Inheritance reworked

--- oop/src/main/kotlin/ru/sber/oop/Inheritance.kt
+++ oop/src/main/kotlin/ru/sber/oop/Inheritance.kt
@@ -1,13 +1,19 @@
 package ru.sber.oop
 
-class Room(val name: String, val size: Int) {
+open class Room(val name: String, val size: Int) {
 
-    val dangerLevel = 5
+    protected open val dangerLevel = 5
 
     fun description() = "Room: $name"
 
-    fun load() = "Nothing much to see here..."
+    open fun load() = "Nothing much to see here..."
 
+    constructor(name: String) : this(name, 100)
 }
 
-//TODO: create class TownSquare here...
+class TownSquare : Room(name = "Town Square", size = 1000) {
+
+    override val dangerLevel = super.dangerLevel - 3
+
+    override fun load() = "The square is the most popular and in the same time the most boring place in the city"
+}
