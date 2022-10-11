ef687a773646fbdad3ad587bd2edba611e58e06d
Georgy K <vangog-2005@ngs.ru>
2022-10-11 20:48:22 +0300

    Sprint-2: io done

--- io/src/main/kotlin/ru/sber/io/Archivator.kt
+++ io/src/main/kotlin/ru/sber/io/Archivator.kt
@@ -1,10 +1,20 @@
 package ru.sber.io
 
+import java.io.File
+import java.io.IOException
+import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
+import java.util.zip.ZipOutputStream
+
 /**
  * Реализовать методы архивации и разархивации файла.
  * Для реализиации использовать ZipInputStream и ZipOutputStream.
  */
 class Archivator {
+
+    private val FILENAME = "./io/logfile.log"
+    private val UNZIPPED_FILENAME = "./io/unzippedLogfile.log"
+    private val ARCHIVENAME = FILENAME.replace(".log", ".zip")
 
     /**
      * Метод, который архивирует файл logfile.log в архив logfile.zip.
@@ -12,6 +22,24 @@ class Archivator {
      */
     fun zipLogfile() {
 
+        try {
+            ZipOutputStream(File(ARCHIVENAME).outputStream()).use { writer ->
+                val file = File(FILENAME)
+                val zipFile = ZipEntry(file.name)
+
+                writer.putNextEntry(zipFile)
+
+                file.inputStream().use {
+                    var readByte = it.read()
+                    while (readByte != -1) {
+                        writer.write(readByte)
+                        readByte = it.read()
+                    }
+                }
+            }
+        } catch (exception: IOException) {
+            println("Something went wrong: ${exception.message}")
+        }
     }
 
     /**
@@ -19,6 +47,22 @@ class Archivator {
      * Извлечь из архива logfile.zip файл и сохарнить его в том же каталоге с именем unzippedLogfile.log
      */
     fun unzipLogfile() {
+        val unzippedFile = File(UNZIPPED_FILENAME)
 
+        try {
+            ZipInputStream(File(ARCHIVENAME).inputStream()).use {
+                it.nextEntry
+
+                unzippedFile.outputStream().use { writer ->
+                    var readByte = it.read()
+                    while (readByte != -1) {
+                        writer.write(readByte)
+                        readByte = it.read()
+                    }
+                }
+            }
+        } catch (exception: IOException) {
+            println("Something went wrong: ${exception.message}")
+        }
     }
 }
