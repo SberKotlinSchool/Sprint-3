40e587cd0ad57a23a687e71d49871e1fa80067a2
Maria Starikova <starikova.mariya.v@gmail.com>
2022-10-13 23:42:43 +0300

    add oop,nio

--- oop/src/main/kotlin/ru/sber/oop/Inheritance.kt
+++ oop/src/main/kotlin/ru/sber/oop/Inheritance.kt
@@ -1,13 +1,30 @@
 package ru.sber.oop
 
-class Room(val name: String, val size: Int) {
+ open class Room(val name: String, val size: Int) {
 
-    val dangerLevel = 5
-
+    open val dangerLevel = 5
     fun description() = "Room: $name"
+    open fun load() = monster.getSalution()
 
-    fun load() = "Nothing much to see here..."
+     constructor(name: String) : this(size = 100, name = name)
+
+     val monster = Goblin("Goblin", 100, "Goblin", "Goblin")
+}
+
+
+//TODO: create class TownSquare here...
+class TownSquare : Room("Town Square", 1000){
+    final override fun load() = "Town Square loading..."
+    override val dangerLevel = super.dangerLevel -2
 
 }
 
-//TODO: create class TownSquare here...
+fun main() {
+    val townSquare = TownSquare()
+    println(townSquare.load())
+    val room = Room("box", 100)
+    println(room.load())
+
+}
+
+
