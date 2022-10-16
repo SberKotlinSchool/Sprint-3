a4ff99d62440aacac3563c46b140c9b704beae9c
DSGNSYSTEM-610119377283 <MENichkov@sberbank.ru>
2022-10-16 17:11:21 +0300

    io, nio, oop

---
+++ io/src/main/kotlin/ru/sber/nio/Main.kt
@@ -0,0 +1,8 @@
+package ru.sber.nio
+
+class Main {
+    fun main(){
+        val grep = Grep()
+        grep.find("22/Jan/2001:14:27:46")
+    }
+}
--- oop/src/main/kotlin/ru/sber/oop/DataClasses.kt
+++ oop/src/main/kotlin/ru/sber/oop/DataClasses.kt
@@ -2,10 +2,20 @@ package ru.sber.oop
 
 data class User(val name: String, val age: Long) {
     lateinit var city: String
+
+    override fun equals(other: Any?): Boolean {
+        if (other !is User) return false
+        return (name != other.name && age != other.age && city != other.city)
+    }
 }
 
 fun main() {
     val user1 = User("Alex", 13)
-    //TODO: user2 = ...
-    //TODO: user3 = ...
+    val user2 = user1.copy("Mikhail")
+
+    user1.city = "Orenburg"
+    val user3 = user1.copy()
+    user3.city = "Moscow"
+
+    println(user1.equals(user3))
 }
