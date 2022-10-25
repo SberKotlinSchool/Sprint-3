ee1c9e0cad401f3b5fb18d9ead1507f35b5f1335
albertincx <albertincx@gmail.com>
2022-10-25 20:08:47 +0200

    CertificateRequestTest

---
+++ qa/src/test/kotlin/ru/sber/qa/CertificateRequestTest.kt
@@ -0,0 +1,33 @@
+package ru.sber.qa
+
+import io.mockk.every
+import io.mockk.mockkObject
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class CertificateRequestTest {
+    private val hrEmployeeNumber = 1L
+
+    private lateinit var certificateRequest: CertificateRequest
+
+    @BeforeEach
+    fun setUp() {
+        certificateRequest = CertificateRequest(hrEmployeeNumber, CertificateType.NDFL)
+        mockkObject(Scanner)
+        every { Scanner.getScanData() } returns ByteArray(1)
+    }
+
+    @Test
+    fun `processedBy valid`() {
+        val certificate = certificateRequest.process(hrEmployeeNumber)
+        assertEquals(certificate.processedBy, hrEmployeeNumber)
+    }
+
+    @Test
+    fun `certificateRequest valid`() {
+        val certificate = certificateRequest.process(hrEmployeeNumber)
+        assertEquals(certificateRequest, certificate.certificateRequest)
+    }
+}
