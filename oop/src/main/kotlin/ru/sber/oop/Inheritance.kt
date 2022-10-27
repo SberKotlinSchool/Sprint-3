f521668acb435cb35240f855fdb1bd0dd65b010a
Korshik <dakorshik@sberbank.ru>
2022-10-27 21:08:17 +0300

    Solve Polymorphism

--- oop/src/main/kotlin/ru/sber/oop/Inheritance.kt
+++ oop/src/main/kotlin/ru/sber/oop/Inheritance.kt
@@ -1,15 +1,24 @@
 package ru.sber.oop
 
+import kotlin.random.Random
+
 open class Room(val name: String, val size: Int) {
 
     protected open val dangerLevel = 5
+    val monster: Monster = Goblin(
+        "Strength",
+        Random.Default.nextInt(1, Int.MAX_VALUE),
+        "Goblin",
+        "Ugly"
+    )
 
     constructor(name: String) : this(name, 100)
 
     fun description() = "Room: $name"
 
-    open fun load() = "Nothing much to see here..."
+    open fun load() = monster.getSalutation()
 
+    fun Monster.getSalutation() = "Hi, buddy!"
 }
 
 class TownSquare : Room(name = "Town Square", size = 1000) {
