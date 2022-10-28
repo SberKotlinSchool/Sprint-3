ec8aa351f76e5006bdda6012acc038bd9e0076df
Korshik <dakorshik@sberbank.ru>
2022-10-28 22:08:27 +0300

    Solve zipLogfile

--- io/src/main/kotlin/ru/sber/io/Archivator.kt
+++ io/src/main/kotlin/ru/sber/io/Archivator.kt
@@ -1,5 +1,12 @@
 package ru.sber.io
 
+import java.io.BufferedOutputStream
+import java.io.File
+import java.io.OutputStream
+import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
+import java.util.zip.ZipOutputStream
+
 /**
  * Реализовать методы архивации и разархивации файла.
  * Для реализиации использовать ZipInputStream и ZipOutputStream.
@@ -11,7 +18,14 @@ class Archivator {
      * Архив должен располагаться в том же каталоге, что и исходной файл.
      */
     fun zipLogfile() {
-
+        val logFile = File("io/logfile.log")
+        val zipFile = File("io/logfile.zip")
+        ZipOutputStream(BufferedOutputStream(zipFile.outputStream(), 2048))
+            .use { zipOut ->
+                zipOut.putNextEntry(ZipEntry(logFile.name))
+                logFile.inputStream().buffered(2048)
+                    .use { data -> data.copyTo(zipOut) }
+            }
     }
 
     /**
