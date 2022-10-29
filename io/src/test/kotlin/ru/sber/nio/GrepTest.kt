49befcb0b08211e025df99d7ea2816d46a2abac9
albertincx <albertincx@gmail.com>
2022-10-29 08:44:17 +0200

    nio

---
+++ io/src/test/kotlin/ru/sber/nio/GrepTest.kt
@@ -0,0 +1,12 @@
+package ru.sber.nio
+
+import org.junit.jupiter.api.Test
+
+internal class GrepTest {
+
+    @Test
+    fun find() {
+        val fund = Grep()
+//        fund.find("22/Jan/2001:14:27:46");
+    }
+}
