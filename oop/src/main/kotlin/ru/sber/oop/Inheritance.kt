2a9da6ff5a7608774a7815c89aa8d666afa84fa3
Aleksey Sypchenko <taler2002@mail.ru>
2022-11-04 17:32:22 +0300

    oop for Sprint-3

--- oop/src/main/kotlin/ru/sber/oop/Inheritance.kt
+++ oop/src/main/kotlin/ru/sber/oop/Inheritance.kt
@@ -1,13 +1,18 @@
 package ru.sber.oop
 
-class Room(val name: String, val size: Int) {
+open class Room(val name: String, val size: Int) {
 
-    val dangerLevel = 5
+    protected open val dangerLevel = 5
+    private val monster: Monster = Goblin("Phis", 1000, "Goblin", "Ugly goblin")
+    constructor(_name: String) : this(name = _name, size = 100)
 
     fun description() = "Room: $name"
 
-    fun load() = "Nothing much to see here..."
-
+    open fun load() = monster.getSalutation()
 }
 
-//TODO: create class TownSquare here...
+class TownSquare : Room("Town Square", 1000) {
+    override val dangerLevel: Int = 2
+
+    override fun load() = "Everything is nothing"
+}
