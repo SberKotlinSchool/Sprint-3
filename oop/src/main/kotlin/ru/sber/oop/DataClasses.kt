7c9a5ebe5bc40ec012e6d9bcb8e1795cffc67507
Anna Popova <anna.popova201909@gmail.com>
2022-11-05 17:35:21 +0300

    edit DataClasses

--- oop/src/main/kotlin/ru/sber/oop/DataClasses.kt
+++ oop/src/main/kotlin/ru/sber/oop/DataClasses.kt
@@ -1,11 +1,24 @@
 package ru.sber.oop
 
+
 data class User(val name: String, val age: Long) {
     lateinit var city: String
+    override fun equals(other: Any?): Boolean {
+        if (other == null || other !is User)
+            return false
+        return name == other.name &&
+                city == other.city &&
+                age == other.age
+    }
 }
 
 fun main() {
     val user1 = User("Alex", 13)
-    //TODO: user2 = ...
-    //TODO: user3 = ...
+    val user2 = user1.copy(name = "Mikhail")
+
+    user1.city = "Omsk"
+    val user3 = user1.copy()
+    user3.city = "Tomsk"
+
+    print(user1 == user3)
 }
