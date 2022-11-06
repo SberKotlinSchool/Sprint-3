6d90cfd601d771bcbc17cec111ee9fb1521dd2d9
Dmitry Biryukov <dmitrybiryukov4@gmail.com>
2022-11-06 17:55:33 +0300

    Testing, OOP and IO are done

--- io/src/main/kotlin/ru/sber/io/Archivator.kt
+++ io/src/main/kotlin/ru/sber/io/Archivator.kt
@@ -1,5 +1,14 @@
 package ru.sber.io
 
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.lang.Exception
+import java.nio.charset.Charset
+import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
+import java.util.zip.ZipOutputStream
+
 /**
  * Реализовать методы архивации и разархивации файла.
  * Для реализиации использовать ZipInputStream и ZipOutputStream.
@@ -10,15 +19,47 @@ class Archivator {
      * Метод, который архивирует файл logfile.log в архив logfile.zip.
      * Архив должен располагаться в том же каталоге, что и исходной файл.
      */
-    fun zipLogfile() {
-
+    fun zipLogfile(fileName: String = "logfile.log", directory: String = "io/", fileNameZip: String = fileName.substring(0,fileName.lastIndexOf('.'))) {
+        lateinit var zipOutputStream: ZipOutputStream
+        try{
+            val file = File("$directory$fileName")
+            val data = file.inputStream().buffered().readBytes()
+            zipOutputStream = ZipOutputStream(FileOutputStream("$directory$fileNameZip.zip"))
+            zipOutputStream.use {
+                zipOutputStream.putNextEntry(ZipEntry(fileName))
+                zipOutputStream.write(data)
+                zipOutputStream.closeEntry()
+            }
+        }catch (e:Exception){
+            print(e.message)
+        }finally {
+            zipOutputStream.close()
+        }
     }
 
     /**
      * Метод, который извлекает файл из архива.
      * Извлечь из архива logfile.zip файл и сохарнить его в том же каталоге с именем unzippedLogfile.log
      */
-    fun unzipLogfile() {
-
+    fun unzipLogfile(fileNameZip: String = "logfile", directory: String = "io/", fileName: String = "unzipped${fileNameZip[0].uppercase()}${fileNameZip.substring(1)}") {
+       lateinit var zipInputStream: ZipInputStream
+        try{
+            zipInputStream = ZipInputStream(FileInputStream("$directory$fileNameZip.zip"))
+            val nextEntry = zipInputStream.nextEntry
+            val fileNameInZip = nextEntry?.name
+            val fileFormat = fileNameInZip?.substring(fileNameInZip.lastIndexOf('.'))
+            val file = File("$directory$fileName$fileFormat")
+            while( nextEntry != null) {
+                val lines = zipInputStream.bufferedReader(Charset.forName("UTF-8")).readLines()
+                val writer = file.writer(Charset.forName("UTF-8"))
+                writer.use { w -> lines.forEach { line -> w.write(line + "\n") } }
+                zipInputStream.closeEntry()
+            }
+        }catch (e:Exception){
+            print(e.message)
+        }finally {
+            zipInputStream.close()
+        }
     }
+
 }
