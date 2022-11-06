4976799ed8b90adcb4bc4c4a78da601f9b871787
Романова Ирина Дмитриевна <IDmRomanova@sberbank.ru>
2022-11-06 20:13:53 +0300

    тесты

--- qa/src/test/kotlin/ru/sber/qa/HrDepartmentTest.kt
+++ qa/src/test/kotlin/ru/sber/qa/HrDepartmentTest.kt
@@ -1,10 +1,15 @@
 package ru.sber.qa
 
+import io.mockk.MockKAnnotations
 import io.mockk.every
+import io.mockk.impl.annotations.MockK
 import io.mockk.mockkStatic
+import io.mockk.spyk
+import io.mockk.verify
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 import java.time.Clock
 import java.time.DayOfWeek
@@ -13,16 +18,16 @@ import java.time.LocalDateTime
 import java.time.ZoneId
 
 internal class HrDepartmentTest {
-    private val employeeNumber = 123L
-    private val certificateType = CertificateType.LABOUR_BOOK
+    @MockK
+    private lateinit var certificateRequest: CertificateRequest
 
     private val clock = Clock.fixed(
         Instant.parse("2022-11-01T23:00:00.00Z"), ZoneId.of("Europe/Moscow"))
 
-    private val certificateRequest = CertificateRequest(employeeNumber, certificateType)
-
     @BeforeEach
     fun init() {
+        MockKAnnotations.init(this)
+        spyk<HrDepartment>()
     }
 
     @Test
@@ -48,7 +53,56 @@ internal class HrDepartmentTest {
     }
 
     @Test
-    fun processNextRequest() {
+    fun receiveRequestNegativeTest2() {
+        mockkStatic(LocalDateTime::class)
+        every { LocalDateTime.now(HrDepartment.clock).dayOfWeek } returns DayOfWeek.THURSDAY
+        every { certificateRequest.certificateType } returns CertificateType.NDFL
+        assertThrows<NotAllowReceiveRequestException> {
+            HrDepartment.receiveRequest(certificateRequest)
+        }
     }
 
+    @Test
+    fun receiveRequestNegativeTest3() {
+        mockkStatic(LocalDateTime::class)
+        every { LocalDateTime.now(HrDepartment.clock).dayOfWeek } returns DayOfWeek.MONDAY
+        every { certificateRequest.certificateType } returns CertificateType.LABOUR_BOOK
+        assertThrows<NotAllowReceiveRequestException> {
+            HrDepartment.receiveRequest(certificateRequest)
+        }
+    }
+
+    @Test
+    fun receiveRequestPositiveTest1() {
+        mockkStatic(LocalDateTime::class)
+        every { LocalDateTime.now(HrDepartment.clock).dayOfWeek } returns DayOfWeek.MONDAY
+        every { certificateRequest.certificateType } returns CertificateType.NDFL
+        assertDoesNotThrow {
+            HrDepartment.receiveRequest(certificateRequest)
+        }
+    }
+
+    @Test
+    fun receiveRequestPositiveTest2() {
+        mockkStatic(LocalDateTime::class)
+        every { LocalDateTime.now(HrDepartment.clock).dayOfWeek } returns DayOfWeek.THURSDAY
+        every { certificateRequest.certificateType } returns CertificateType.LABOUR_BOOK
+        assertDoesNotThrow {
+            HrDepartment.receiveRequest(certificateRequest)
+        }
+    }
+
+    @Test
+    fun processNextRequest() {
+        mockkStatic(LocalDateTime::class)
+        every { LocalDateTime.now(HrDepartment.clock).dayOfWeek } returns DayOfWeek.THURSDAY
+        every { certificateRequest.certificateType } returns CertificateType.LABOUR_BOOK
+        HrDepartment.receiveRequest(certificateRequest)
+
+        every { certificateRequest.process(any()) } returns Certificate(certificateRequest, 123L, ByteArray(1))
+        HrDepartment.processNextRequest(123L)
+        verify {
+            certificateRequest.process(123L)
+        }
+    }
 }
