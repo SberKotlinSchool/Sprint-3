a50d079c6b973e6b88fdb0c3b6d97e010c8af543
albertincx <albertincx@gmail.com>
2022-11-08 07:46:43 +0300

    verify scanner new processNextRequest

--- qa/src/test/kotlin/ru/sber/qa/HrDepartmentTest.kt
+++ qa/src/test/kotlin/ru/sber/qa/HrDepartmentTest.kt
@@ -1,9 +1,7 @@
 package ru.sber.qa
 
 import io.mockk.*
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.*
 import java.time.Clock
 import java.time.Instant
 import java.time.ZoneId
@@ -11,7 +9,7 @@ import java.time.ZoneId
 fun getClock(time: String): Clock =
     Clock.fixed(Instant.parse(time), ZoneId.of("UTC"))
 
-internal class HrDepartmentTest {
+class HrDepartmentTest {
     private val certificateRequest = mockk<CertificateRequest>()
 
     @AfterEach
@@ -21,39 +19,56 @@ internal class HrDepartmentTest {
 
     @Test
     fun `receiveRequest Throws WeekendDayException`() {
+        HrDepartment.clock = getClock("2022-10-30T10:15:30.00Z")
+
         assertThrows<WeekendDayException> {
-            HrDepartment.clock = getClock("2022-10-30T10:15:30.00Z")
             HrDepartment.receiveRequest(certificateRequest)
         }
     }
 
     @Test
     fun `first throw NotAllowReceiveRequestException`() {
+        HrDepartment.clock = getClock("2022-10-25T10:15:30.00Z") //TUESDAY
+        every { certificateRequest.certificateType } returns CertificateType.NDFL
+
         assertThrows<NotAllowReceiveRequestException> {
-            HrDepartment.clock = getClock("2022-10-25T10:15:30.00Z") //TUESDAY
-            every { certificateRequest.certificateType } returns CertificateType.NDFL
             HrDepartment.receiveRequest(certificateRequest)
         }
     }
 
     @Test
     fun `second throw NotAllowReceiveRequestException`() {
+        HrDepartment.clock = getClock("2022-10-24T10:15:30.00Z") //MONDAY
+        every { certificateRequest.certificateType } returns CertificateType.LABOUR_BOOK
+
         assertThrows<NotAllowReceiveRequestException> {
-            HrDepartment.clock = getClock("2022-10-24T10:15:30.00Z") //MONDAY
-            every { certificateRequest.certificateType } returns CertificateType.LABOUR_BOOK
             HrDepartment.receiveRequest(certificateRequest)
         }
     }
 
-    @Test
-    fun `success push and poll`() {
-        HrDepartment.clock = getClock("2022-10-24T10:15:30.00Z") //MONDAY
-        val hrEmployeeNumber = 1L
-        mockkObject(Scanner)
-        every { Scanner.getScanData() } returns ByteArray(1)
-        val certificateR = CertificateRequest(hrEmployeeNumber, CertificateType.NDFL)
-        HrDepartment.receiveRequest(certificateR)
-        HrDepartment.processNextRequest(hrEmployeeNumber)
-        verify { certificateR.process(hrEmployeeNumber) }
+    class NestedOne {
+        private val hrEmployeeNumber = 1L
+
+        @BeforeEach
+        fun setUp() {
+            HrDepartment.clock = getClock("2022-10-24T10:15:30.00Z") //MONDAY
+            mockkObject(Scanner)
+            every { Scanner.getScanData() } returns ByteArray(1)
+            val certificateR = CertificateRequest(hrEmployeeNumber, CertificateType.NDFL)
+            HrDepartment.receiveRequest(certificateR)
+        }
+
+        @AfterEach
+        fun tearDown() {
+            unmockkAll()
+        }
+
+        @Test
+        fun `success processNextRequest`() {
+            val certificateR = CertificateRequest(hrEmployeeNumber, CertificateType.NDFL)
+            HrDepartment.processNextRequest(hrEmployeeNumber)
+            verify { certificateR.process(hrEmployeeNumber) }
+        }
     }
 }
+
