d40f13845c8b2b8b0dd63bdd9cad6d4760d7d11e
Aleksey Sypchenko <taler2002@mail.ru>
2022-11-08 22:50:25 +0300

    Sprint-3 - исправление замечаний код-ревью по "Тестированию" - IO архивирование, разархивирование

--- io/src/main/kotlin/ru/sber/io/Archivator.kt
+++ io/src/main/kotlin/ru/sber/io/Archivator.kt
@@ -1,17 +1,41 @@
 package ru.sber.io
 
+import java.io.File
+import java.io.FileInputStream
+import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
+import java.util.zip.ZipOutputStream
+
 /**
  * Реализовать методы архивации и разархивации файла.
  * Для реализиации использовать ZipInputStream и ZipOutputStream.
  */
 class Archivator {
 
+    private val fileName = "io/logfile.log"
+    private val zipFileName = "io/logfile.zip"
+    private val unZipFileName = "io/unzippedLogfile.log"
     /**
      * Метод, который архивирует файл logfile.log в архив logfile.zip.
      * Архив должен располагаться в том же каталоге, что и исходной файл.
      */
     fun zipLogfile() {
+        val source = File(fileName)
+        val fis = source.inputStream()
 
+        val destination = File(zipFileName)
+        val fos = destination.outputStream()
+        val zipOutputStream = ZipOutputStream(fos, Charsets.UTF_8)
+        val zipEntry = ZipEntry(source.name)
+        zipOutputStream.putNextEntry(zipEntry)
+        val bytes = ByteArray(1024)
+        var length: Int
+        while (fis.read(bytes).also { length = it } >= 0) {
+            zipOutputStream.write(bytes, 0, length)
+        }
+        zipOutputStream.close()
+        fis.close()
+        fos.close()
     }
 
     /**
@@ -19,6 +43,22 @@ class Archivator {
      * Извлечь из архива logfile.zip файл и сохарнить его в том же каталоге с именем unzippedLogfile.log
      */
     fun unzipLogfile() {
-
+        val destination = File(unZipFileName)
+        val buffer = ByteArray(1024)
+        val zipInputStream = ZipInputStream(FileInputStream(zipFileName))
+        val fos = destination.outputStream()
+        var length: Int
+        while (zipInputStream.read(buffer).also { length = it } > 0) {
+            fos.write(buffer, 0, length)
+        }
+        zipInputStream.closeEntry()
+        zipInputStream.close()
+        fos.close()
     }
+}
+
+fun main () {
+    Archivator().zipLogfile()
+    Archivator().unzipLogfile()
+
 }
--- qa/src/test/kotlin/ru/sber/qa/HrDepartmentTest.kt
+++ qa/src/test/kotlin/ru/sber/qa/HrDepartmentTest.kt
@@ -35,41 +35,29 @@ internal class HrDepartmentTest {
     @Test
     fun `test receiveRequest correct output when DayOfWeek equals MONDAY and CertificateType equals NDFL`() {
         //given
-        val incomeBox: LinkedList<CertificateRequest> = LinkedList()
         HrDepartment.clock = Clock.fixed(Instant.parse("2022-11-07T10:00:00.00Z"), ZoneId.of("Europe/Moscow")) //MONDAY
+        val incomeBoxList = getPrivateFieldIncomeBoxInHrDepartment()
 
         //when
         every { certificateRequest.certificateType } returns CertificateType.NDFL
         HrDepartment.receiveRequest(certificateRequest)
 
         //then
-        HrDepartment.javaClass.getDeclaredField("incomeBox").let {
-            it.isAccessible = true
-            val incomeBoxList = it.get(incomeBox)
-            if (incomeBoxList is LinkedList<*>) {
-                assertEquals(certificateRequest, incomeBoxList.first)
-            }
-        }
+        assertEquals(certificateRequest, incomeBoxList.first)
     }
 
     @Test
     fun `test receiveRequest correct output when DayOfWeek equals TUESDAY and CertificateType equals LABOUR_BOOK`() {
         //given
-        val incomeBox: LinkedList<CertificateRequest> = LinkedList()
         HrDepartment.clock = Clock.fixed(Instant.parse("2022-11-08T10:00:00.00Z"), ZoneId.of("Europe/Moscow")) //TUESDAY
+        val incomeBoxList = getPrivateFieldIncomeBoxInHrDepartment()
 
         //when
         every { certificateRequest.certificateType } returns CertificateType.LABOUR_BOOK
         HrDepartment.receiveRequest(certificateRequest)
 
         //then
-        HrDepartment.javaClass.getDeclaredField("incomeBox").let {
-            it.isAccessible = true
-            val incomeBoxList = it.get(incomeBox)
-            if (incomeBoxList is LinkedList<*>) {
-                assertEquals(certificateRequest, incomeBoxList.first)
-            }
-        }
+        assertEquals(certificateRequest, incomeBoxList.first)
     }
 
     @Test
@@ -98,22 +86,29 @@ internal class HrDepartmentTest {
         //given
         val certificate = mockk<Certificate>()
         every { certificateRequest.process(0L) } returns certificate
-        val incomeBox: LinkedList<CertificateRequest> = LinkedList()
         val outcomeOutcome: LinkedList<Certificate> = LinkedList()
-        HrDepartment.javaClass.getDeclaredField("incomeBox").let {
-            it.isAccessible = true
-            val incomeBoxList = it.get(incomeBox) as LinkedList<CertificateRequest>
-            incomeBoxList.push(certificateRequest)
-        }
-
-        //then
+        val incomeBoxList = getPrivateFieldIncomeBoxInHrDepartment() as LinkedList<CertificateRequest>
+        incomeBoxList.push(certificateRequest)
+        var outcomeOutcomeList: LinkedList<Certificate>
         HrDepartment.javaClass.getDeclaredField("outcomeOutcome").let {
             it.isAccessible = true
-            val outcomeOutcomeList = it.get(outcomeOutcome) as LinkedList<*>
-
-            HrDepartment.processNextRequest(0L)
-
-            assertEquals(certificate, outcomeOutcomeList.first)
+            outcomeOutcomeList = it.get(outcomeOutcome) as LinkedList<Certificate>
         }
+
+        //when
+        HrDepartment.processNextRequest(0L)
+
+        //then
+        assertEquals(certificate, outcomeOutcomeList.first)
+    }
+
+    private fun getPrivateFieldIncomeBoxInHrDepartment(): LinkedList<*> {
+        val incomeBox: LinkedList<CertificateRequest> = LinkedList()
+        val incomeBoxList: LinkedList<*>
+        HrDepartment.javaClass.getDeclaredField("incomeBox").let {
+            it.isAccessible = true
+            incomeBoxList = it.get(incomeBox) as LinkedList<*>
+        }
+        return incomeBoxList
     }
 }
