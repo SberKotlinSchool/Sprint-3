190c53e563c58fc72f94dac6595a67031beaf6b5
Романова Ирина Дмитриевна <IDmRomanova@sberbank.ru>
2022-11-08 21:59:28 +0300

    io

--- io/src/main/kotlin/ru/sber/io/Archivator.kt
+++ io/src/main/kotlin/ru/sber/io/Archivator.kt
@@ -1,17 +1,34 @@
 package ru.sber.io
 
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
+import java.util.zip.ZipOutputStream
+
 /**
  * Реализовать методы архивации и разархивации файла.
  * Для реализиации использовать ZipInputStream и ZipOutputStream.
  */
 class Archivator {
 
+    private val logFile = File("io/logfile.log")
+    private val zipFile = File("io/logfile.zip")
+    private val unzipFile = File("io/unzippedLogfile.log")
+
     /**
      * Метод, который архивирует файл logfile.log в архив logfile.zip.
      * Архив должен располагаться в том же каталоге, что и исходной файл.
      */
     fun zipLogfile() {
-
+        ZipOutputStream(zipFile.outputStream()).use { output ->
+            FileInputStream(logFile).use { file ->
+                val entry = ZipEntry(logFile.name)
+                output.putNextEntry(entry)
+                file.copyTo(output)
+            }
+        }
     }
 
     /**
@@ -19,6 +36,11 @@ class Archivator {
      * Извлечь из архива logfile.zip файл и сохарнить его в том же каталоге с именем unzippedLogfile.log
      */
     fun unzipLogfile() {
-
+        FileOutputStream(unzipFile).use { file ->
+            ZipInputStream(zipFile.inputStream()).use { input ->
+                val entry = input.nextEntry
+                if (entry != null) input.copyTo(file)
+            }
+        }
     }
 }
