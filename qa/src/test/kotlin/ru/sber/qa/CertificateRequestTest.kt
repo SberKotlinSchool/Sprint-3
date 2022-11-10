022481395af4a493764b4c9af4c67371f5d2d398
Tatiana Dolgopolova <tdolgopol@mail.ru>
2022-11-10 15:51:32 +0600

    Исправлены замечания по QA

--- qa/src/test/kotlin/ru/sber/qa/CertificateRequestTest.kt
+++ qa/src/test/kotlin/ru/sber/qa/CertificateRequestTest.kt
@@ -2,6 +2,7 @@ package ru.sber.qa
 
 import io.mockk.every
 import io.mockk.mockkObject
+import io.mockk.verify
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -10,7 +11,7 @@ import kotlin.random.Random
 
 class CertificateRequestTest {
 
-    private val employeeNumber : Long = 10
+    private val employeeNumber: Long = 10
     private val certificateType = CertificateType.LABOUR_BOOK
     private val scanData = Random.nextBytes(100)
     private val certRequest = CertificateRequest(employeeNumber, certificateType)
@@ -22,16 +23,11 @@ class CertificateRequestTest {
     }
 
     @Test
-    fun testBase() {
-        assertEquals(certificateType, certRequest.certificateType)
-        assertEquals(employeeNumber, certRequest.employeeNumber)
-    }
-
-    @Test
     fun testProcess() {
         val cert = certRequest.process(employeeNumber)
         assertEquals(certRequest, cert.certificateRequest)
         assertEquals(employeeNumber, cert.processedBy)
         assertEquals(scanData, cert.data)
+        verify(exactly = 1) { Scanner.getScanData() }
     }
 }
--- qa/src/test/kotlin/ru/sber/qa/HrDepartmentTest.kt
+++ qa/src/test/kotlin/ru/sber/qa/HrDepartmentTest.kt
@@ -3,23 +3,30 @@ package ru.sber.qa
 import io.mockk.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import java.time.Clock
-import java.time.LocalDateTime
-import java.time.ZoneId
-import java.time.ZoneOffset
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.lang.reflect.Field
+import java.time.*
+import java.util.*
+import java.util.stream.Stream
+import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
 
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class HrDepartmentTest {
 
     private val employeeNumber: Long = 10
     private val certRequestMock = mockk<CertificateRequest>()
     private val certMock = mockk<Certificate>()
+    private val hrDep = spyk(HrDepartment, recordPrivateCalls = true)
 
     @BeforeEach
     fun setUp() {
         every { certRequestMock.process(employeeNumber) } returns certMock
-    }
+     }
 
     @Test
     fun testReceiveRequesSunday() {
@@ -40,14 +47,38 @@ class HrDepartmentTest {
         ) { HrDepartment.receiveRequest(certRequestMock) }
     }
 
+    @ParameterizedTest(name = "{index} => type={0}, instant={1}")
+    @MethodSource("paramProvider")
+    fun testReceiveRequest(type: CertificateType, instant: Instant) {
+        HrDepartment.clock = Clock.fixed(instant, ZoneId.of("UTC"))
+        every { certRequestMock.certificateType } returns type
+        val incomeBox = getIncomeBoxProperty().get(HrDepartment) as LinkedList<CertificateRequest>
+        val incomeBoxCountIn = incomeBox.count()
+        HrDepartment.receiveRequest(certRequestMock)
+        val incomeBoxCountOut = incomeBox.count()
+        assertEquals(incomeBoxCountIn + 1, incomeBoxCountOut)
+    }
+
+    private fun paramProvider(): Stream<Arguments?> =  Stream.of(
+        Arguments.of(CertificateType.NDFL, LocalDateTime.of(2022, 10, 31, 15, 8, 0).toInstant(ZoneOffset.UTC)),
+        Arguments.of(CertificateType.LABOUR_BOOK, LocalDateTime.of(2022, 11, 10, 15, 8, 0).toInstant(ZoneOffset.UTC))
+    )
+
     @Test
-    fun testReceiveRequest() {
+    fun testProcessNextRequest() {
         val instant = LocalDateTime.of(2022, 10, 31, 15, 8, 0).toInstant(ZoneOffset.UTC)
         HrDepartment.clock = Clock.fixed(instant, ZoneId.of("UTC"))
         every { certRequestMock.certificateType } returns CertificateType.NDFL
-        HrDepartment.receiveRequest(certRequestMock)
+        val incomeBox = getIncomeBoxProperty().get(HrDepartment) as LinkedList<CertificateRequest>
+        incomeBox.push(certRequestMock)
         HrDepartment.processNextRequest(employeeNumber)
         verify(exactly = 1) { certRequestMock.process(employeeNumber) }
+    }
+
+    private fun getIncomeBoxProperty(): Field {
+        val prop = HrDepartment.javaClass.getDeclaredField("incomeBox")
+        prop.isAccessible = true
+        return prop
     }
 
 }
