c7431a99ce48e2d78e95d95da529e8c26e370de5
Aleksey Sypchenko <taler2002@mail.ru>
2022-11-11 23:08:10 +0300

    Sprint-3 NIO

--- io/src/main/kotlin/ru/sber/nio/Grep.kt
+++ io/src/main/kotlin/ru/sber/nio/Grep.kt
@@ -1,5 +1,13 @@
 package ru.sber.nio
 
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.nio.file.StandardOpenOption
+import kotlin.io.path.isDirectory
+import kotlin.io.path.name
+import kotlin.io.path.useLines
+
 /**
  * Реализовать простой аналог утилиты grep с использованием калссов из пакета java.nio.
  */
@@ -15,6 +23,27 @@ class Grep {
      * 22-01-2001-1.log : 3 : 192.168.1.1 - - [22/Jan/2001:14:27:46 +0000] "POST /files HTTP/1.1" 200 - "-"
      */
     fun find(subString: String) {
-
+        val result = Paths.get("io/result.txt")
+        val path = Paths.get("io/logs")
+        Files.writeString(result, "", StandardCharsets.UTF_8, StandardOpenOption.CREATE)
+        Files.walk(path).filter { !it.isDirectory() }.forEach { file ->
+            file.useLines {
+                it.mapIndexed { index, s ->
+                    if (s.contains(subString)) {
+                        Files.writeString(
+                            result,
+                            file.name + " : " + index + " : " + s + "\n",
+                            StandardCharsets.UTF_8,
+                            StandardOpenOption.APPEND
+                        )
+                    }
+                }.toList()
+            }
+        }
     }
+}
+
+fun main(){
+    val grep = Grep()
+    grep.find("54.22.12.8")
 }
