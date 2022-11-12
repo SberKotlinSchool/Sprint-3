b4605c4eb2eca6f0f3a29b0a29782267c26471a1
Slabko Konstantin <SirKot@yandex.ru>
2022-11-12 19:37:16 +0300

    Grep solved.

--- io/src/main/kotlin/ru/sber/nio/Grep.kt
+++ io/src/main/kotlin/ru/sber/nio/Grep.kt
@@ -1,9 +1,18 @@
 package ru.sber.nio
 
+import java.nio.file.Files
+import java.nio.file.Paths
+import kotlin.io.path.readLines
+
+
 /**
  * Реализовать простой аналог утилиты grep с использованием калссов из пакета java.nio.
  */
 class Grep {
+
+    private val resultDir = "io/logs"
+    private val resultFile = "io/result.txt"
+
     /**
      * Метод должен выполнить поиск подстроки subString во всех файлах каталога logs.
      * Каталог logs размещен в данном проекте (io/logs) и внутри содержит другие каталоги.
@@ -15,6 +24,23 @@ class Grep {
      * 22-01-2001-1.log : 3 : 192.168.1.1 - - [22/Jan/2001:14:27:46 +0000] "POST /files HTTP/1.1" 200 - "-"
      */
     fun find(subString: String) {
+        val outFile = Paths.get(resultFile).toFile()
 
+        // пересоздадим файл,если он уже существует
+        if (outFile.exists())
+            outFile.delete()
+
+        Files.walk(Paths.get(resultDir)).filter {
+            Files.isRegularFile(it)
+        }.forEach {file ->
+            file.readLines().mapIndexed { index, s ->
+                if (s.contains(subString))
+                    outFile.appendText("${file.fileName} : ${index + 1} : $s\n")
+            }
+        }
     }
+}
+
+fun main() {
+    Grep().find("66.54.0.1")
 }
