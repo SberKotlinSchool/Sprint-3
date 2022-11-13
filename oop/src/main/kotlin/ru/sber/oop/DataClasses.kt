b6354e6d859adfd6050e10cf4a048c1a72b9f674
Dmitry Biryukov <dmitrybiryukov4@gmail.com>
2022-11-13 19:37:49 +0300

    Testing, OOP, IO and NIO are done

--- oop/src/main/kotlin/ru/sber/oop/DataClasses.kt
+++ oop/src/main/kotlin/ru/sber/oop/DataClasses.kt
@@ -5,7 +5,7 @@ data class User(val name: String, val age: Long, val _city: String) {}
 fun main() {
     val user1 = User("Alex", 13, "Omsk")
     //TODO: user2 = ...
-    val user2 = user1.copy("James", 25, "fg")
+    val user2 = user1.copy("James")
     //TODO: user3 = ...
     val user3 = User(user1.name, "${user1.age}".toLong(), "Tomsk")
     println(user1 == user3)
