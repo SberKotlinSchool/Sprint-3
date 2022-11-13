5f4255cf09a2284963e964604f9ae416e44b7867
16939749 / astafyev-ai <astafyev.a.igor@sberbank.ru>
2022-11-13 14:34:16 +0300

    выполнено 2 задание по ООП

--- oop/src/main/kotlin/ru/sber/oop/Inheritance.kt
+++ oop/src/main/kotlin/ru/sber/oop/Inheritance.kt
@@ -1,13 +1,22 @@
 package ru.sber.oop
 
-class Room(val name: String, val size: Int) {
+open class Room(val name: String, val size: Int) {
+    protected open val dangerLevel = 5
 
-    val dangerLevel = 5
+    constructor(name: String) : this(name = name, size = 100)
 
     fun description() = "Room: $name"
 
-    fun load() = "Nothing much to see here..."
-
+    open fun load() = "Nothing much to see here..."
 }
 
 //TODO: create class TownSquare here...
+open class TownSquare : Room(name = "Town Square", size = 1000) {
+    override val dangerLevel: Int
+        get() = super.dangerLevel - 3
+
+    final override fun load(): String {
+        return "Loading..."
+    }
+}
+
