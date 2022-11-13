3cc3475c132bc2622b6d53e2014d4fa6a3814bf8
16939749 / astafyev-ai <astafyev.a.igor@sberbank.ru>
2022-11-13 21:19:13 +0300

    выполнено задание IO (архиватор)

--- io/src/main/kotlin/ru/sber/io/Archivator.kt
+++ io/src/main/kotlin/ru/sber/io/Archivator.kt
@@ -1,17 +1,33 @@
 package ru.sber.io
 
+import java.io.*
+import java.util.zip.*
+
 /**
  * Реализовать методы архивации и разархивации файла.
  * Для реализиации использовать ZipInputStream и ZipOutputStream.
  */
 class Archivator {
+    private val parentDir = "io"
+    private val logFile = File(parentDir, "logfile.log")
+    private val zipFile = File(parentDir, "logfile.zip")
+    private val unzipFile = File(parentDir, "unzippedLogfile.log")
+    private val bufferSize = 1024
 
     /**
      * Метод, который архивирует файл logfile.log в архив logfile.zip.
      * Архив должен располагаться в том же каталоге, что и исходной файл.
      */
     fun zipLogfile() {
-
+        logFile.inputStream()
+            .use { inputStream ->
+                ZipOutputStream(zipFile.outputStream())
+                    .use { zipOutputStream ->
+                        val zipEntry = ZipEntry(logFile.name)
+                        zipOutputStream.putNextEntry(zipEntry)
+                        inputStream.copyTo(zipOutputStream, bufferSize)
+                    }
+            }
     }
 
     /**
@@ -19,6 +35,16 @@ class Archivator {
      * Извлечь из архива logfile.zip файл и сохарнить его в том же каталоге с именем unzippedLogfile.log
      */
     fun unzipLogfile() {
+        ZipInputStream(zipFile.inputStream())
+            .use { zipInputStream ->
 
+                while (zipInputStream.nextEntry != null) {
+                    unzipFile.outputStream()
+                        .use { outputStream ->
+                            zipInputStream.copyTo(outputStream, bufferSize)
+                        }
+                }
+            }
     }
 }
+
