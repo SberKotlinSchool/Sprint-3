154031e32927f6b69d6b6d15607204e046ce62b2
Mikhail Besman <mvbesman@gmail.com>
2022-11-15 00:05:37 +0300

    All

--- io/src/main/kotlin/ru/sber/io/Archivator.kt
+++ io/src/main/kotlin/ru/sber/io/Archivator.kt
@@ -1,5 +1,11 @@
 package ru.sber.io
 
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
+import java.util.zip.ZipOutputStream
+
 /**
  * Реализовать методы архивации и разархивации файла.
  * Для реализиации использовать ZipInputStream и ZipOutputStream.
@@ -10,15 +16,41 @@ class Archivator {
      * Метод, который архивирует файл logfile.log в архив logfile.zip.
      * Архив должен располагаться в том же каталоге, что и исходной файл.
      */
-    fun zipLogfile() {
+    fun zipLogfile(path : String) {
+        val fileInputStream = FileInputStream(path)
 
+        val byteArrayBuffer = ByteArray(fileInputStream.available())
+        fileInputStream.read(byteArrayBuffer)
+        fileInputStream.close()
+
+        val zipOutputStream = ZipOutputStream(FileOutputStream(path.replace(".log" , ".zip")))
+        val zipEntry = ZipEntry(path)
+        zipOutputStream.putNextEntry(zipEntry)
+        zipOutputStream.write(byteArrayBuffer)
+        zipOutputStream.closeEntry()
+        zipOutputStream.close()
     }
 
     /**
      * Метод, который извлекает файл из архива.
      * Извлечь из архива logfile.zip файл и сохарнить его в том же каталоге с именем unzippedLogfile.log
      */
-    fun unzipLogfile() {
+    fun unzipLogfile(path: String) {
+        val zipInputStream = ZipInputStream(FileInputStream(path))
+        zipInputStream.nextEntry
+        val logFileName = path.split("\\").last()
+        val unzippedLogFileName = "unzipped" + logFileName.replace(".zip" , ".log")
+        val fileOutputStream = FileOutputStream(path.replace(logFileName, unzippedLogFileName))
 
+        var nextByte: Int = zipInputStream.read()
+        while (nextByte != -1){
+            fileOutputStream.write(nextByte)
+            nextByte = zipInputStream.read()
+        }
+
+        fileOutputStream.flush()
+        fileOutputStream.close()
+        zipInputStream.closeEntry()
+        zipInputStream.close()
     }
 }
--- oop/src/main/kotlin/ru/sber/oop/Polymorphism.kt
+++ oop/src/main/kotlin/ru/sber/oop/Polymorphism.kt
@@ -1,13 +1,58 @@
 package ru.sber.oop
 
+import kotlin.random.Random
+
 interface Fightable {
     val powerType: String
     var healthPoints: Int
     val damageRoll: Int
+        get() = Random.nextInt(0, 50)
 
     fun attack(opponent: Fightable): Int
 }
 
-//TODO: create class Player, Monster, Goblin here...
+class Player (
+    override val powerType: String,
+    override var healthPoints: Int,
+    override val damageRoll: Int,
+    val name : String,
+    var isBlessed : Boolean
+    ) : Fightable {
+    override fun attack(opponent: Fightable): Int {
+        var damageThisRound = damageRoll
+        if (isBlessed)
+            damageThisRound *= 2
+        opponent.healthPoints -= damageThisRound
+        return damageThisRound
+    }
+}
+
+abstract class Monster  (
+    override val powerType: String,
+    override var healthPoints: Int,
+    open val name : String,
+    open val description : String
+    ) : Fightable {
+    override val damageRoll
+        get() = super.damageRoll
+    override fun attack(opponent: Fightable): Int {
+        var damageThisRound = damageRoll
+        opponent.healthPoints -= damageThisRound
+        return damageThisRound
+    }
+}
+
+class Goblin (
+    override val powerType: String,
+    override var healthPoints: Int,
+    override val name : String,
+    override val description : String
+        ) : Monster(powerType, healthPoints, name, description) {
+
+    override val damageRoll
+        get() = super.damageRoll / 2
+
+}
+
 
 
