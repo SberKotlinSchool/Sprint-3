cc150a94a4d7b2670d487f54c4423579a642dd1f
Anna Popova <anna.popova201909@gmail.com>
2022-11-15 11:29:55 +0300

    fix User constructor

--- oop/src/main/kotlin/ru/sber/oop/DataClasses.kt
+++ oop/src/main/kotlin/ru/sber/oop/DataClasses.kt
@@ -1,23 +1,12 @@
 package ru.sber.oop
 
 
-data class User(val name: String, val age: Long) {
-    lateinit var city: String
-
-    override fun equals(other: Any?): Boolean {
-        if (other == null || other !is User || !this::city.isInitialized)
-            return false
-        return name == other.name &&
-                city == other.city &&
-                age == other.age
-    }
-}
+data class User(val name: String, val age: Long, var city: String)
 
 fun main() {
-    val user1 = User("Alex", 13)
+    val user1 = User("Alex", 13, "Omsk")
     val user2 = user1.copy(name = "Mikhail")
 
-    user1.city = "Omsk"
     val user3 = user1.copy()
     user3.city = "Tomsk"
 
