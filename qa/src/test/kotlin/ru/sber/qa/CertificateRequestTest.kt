1d3a24e4e71ee5f09b3e3986589ca21ffa9ceeb7
Anna Popova <anna.popova201909@gmail.com>
2022-11-16 12:16:40 +0300

    QA fix ScannerTest

--- qa/src/test/kotlin/ru/sber/qa/CertificateRequestTest.kt
+++ qa/src/test/kotlin/ru/sber/qa/CertificateRequestTest.kt
@@ -4,7 +4,6 @@ import io.mockk.every
 import io.mockk.mockkObject
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
--- qa/src/test/kotlin/ru/sber/qa/ScannerTest.kt
+++ qa/src/test/kotlin/ru/sber/qa/ScannerTest.kt
@@ -1,14 +1,7 @@
 package ru.sber.qa
 
 import org.junit.jupiter.api.Test
-
 import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.MethodSource
-import java.time.Clock
-import java.time.Instant
-import java.time.ZoneId
-import java.util.Scanner
 import kotlin.test.assertFailsWith
 
 
@@ -24,7 +17,7 @@ internal class ScannerTest {
     @Test
     fun shouldGetWeekendDayException() {
         assertFailsWith<ScanTimeoutException>("Не получено корректное исключение ScanTimeoutException") {
-           val scanner = Scanner
+            val scanner = Scanner
             scanner.getScanData(1000L)
         }
     }
