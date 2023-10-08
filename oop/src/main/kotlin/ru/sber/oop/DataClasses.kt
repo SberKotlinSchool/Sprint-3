4072734ea1e6b12c019bdff867fcc85e30c143f7
ShedowSith <rahimyanov_aleks@mail.ru>
2023-10-08 19:45:21 +0300

    Выполнение ДЗ

--- oop/src/main/kotlin/ru/sber/oop/DataClasses.kt
+++ oop/src/main/kotlin/ru/sber/oop/DataClasses.kt
@@ -2,10 +2,27 @@ package ru.sber.oop
 
 data class User(val name: String, val age: Long) {
     lateinit var city: String
+    override fun equals(other: Any?): Boolean {
+        if (other == null || this::class != other::class) return false
+        val obj = other as User
+        return this.name == other.name && this.age == obj.age && this.city == obj.city
+    }
+
+    override fun hashCode(): Int {
+        var result = name.hashCode()
+        result = 31 * result + age.hashCode()
+        result = 31 * result + city.hashCode()
+        return result
+    }
 }
 
 fun main() {
     val user1 = User("Alex", 13)
-    //TODO: user2 = ...
-    //TODO: user3 = ...
+    val user2 = user1.copy(name = "Lex")
+
+    user1.city = "Omsk"
+
+    val user3 = user1.copy().let { it.city = "Tomsk" }
+
+    println(user1.equals(user3))
 }
--- oop/src/main/kotlin/ru/sber/oop/Inheritance.kt
+++ oop/src/main/kotlin/ru/sber/oop/Inheritance.kt
@@ -1,13 +1,21 @@
 package ru.sber.oop
 
-class Room(val name: String, val size: Int) {
+open class Room(val name: String, val size: Int) {
 
-    val dangerLevel = 5
+    protected open val dangerLevel = 5
 
+    private val monster = Goblin("Слабый", 50, "Гоша", "Гоблин")
+    constructor(name: String): this(name, 100)
     fun description() = "Room: $name"
 
-    fun load() = "Nothing much to see here..."
+    open fun load() = monster.getSalutation()
 
 }
 
-//TODO: create class TownSquare here...
+class TownSquare(): Room("TownSquare", 1000) {
+    override val dangerLevel: Int
+        get() = super.dangerLevel - 3
+    final override fun load() = "Просто загрузка"
+}
+
+fun  Monster.getSalutation() = "Привет, я дружелюбный монстр"
