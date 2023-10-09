c71230b22b9570fdbcd995080f0b4e96190eb1b6
ShedowSith <rahimyanov_aleks@mail.ru>
2023-10-09 18:23:03 +0300

    Реализация архиватора

--- io/src/main/kotlin/ru/sber/io/Archivator.kt
+++ io/src/main/kotlin/ru/sber/io/Archivator.kt
@@ -1,17 +1,40 @@
 package ru.sber.io
 
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
+import java.util.zip.ZipOutputStream
+
+
 /**
  * Реализовать методы архивации и разархивации файла.
  * Для реализиации использовать ZipInputStream и ZipOutputStream.
  */
 class Archivator {
+    private val fileName = "logfile.log"
+    private val zipFileName = "logfile.zip"
+    private val parent = "io"
 
     /**
      * Метод, который архивирует файл logfile.log в архив logfile.zip.
      * Архив должен располагаться в том же каталоге, что и исходной файл.
      */
     fun zipLogfile() {
-
+        try {
+            ZipOutputStream(FileOutputStream("$parent/$zipFileName")).use { zipOut ->
+                FileInputStream("$parent/$fileName").use { fis ->
+                    val entry = ZipEntry(fileName)
+                    zipOut.putNextEntry(entry)
+                    val buffer = ByteArray(fis.available())
+                    fis.read(buffer)
+                    zipOut.write(buffer)
+                    zipOut.closeEntry()
+                }
+            }
+        } catch (ex: Exception) {
+            println(ex.message)
+        }
     }
 
     /**
@@ -19,6 +42,22 @@ class Archivator {
      * Извлечь из архива logfile.zip файл и сохарнить его в том же каталоге с именем unzippedLogfile.log
      */
     fun unzipLogfile() {
-
+        try {
+            ZipInputStream(FileInputStream("$parent/$zipFileName")).use { zipIn ->
+                while (zipIn.getNextEntry() != null) {
+                    val fileOut = FileOutputStream("$parent/unzipped$fileName")
+                    var data = zipIn.read()
+                    while (data != -1) {
+                        fileOut.write(data)
+                        data = zipIn.read()
+                    }
+                    fileOut.flush()
+                    zipIn.closeEntry()
+                    fileOut.close()
+                }
+            }
+        } catch (ex: Exception) {
+            println(ex.message)
+        }
     }
 }
