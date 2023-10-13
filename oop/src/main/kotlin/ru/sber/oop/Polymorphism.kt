672133de45c241a9f60d4c9e4740da9e5d6c9b55
avkononenko <avkononenko@domclick.ru>
2023-10-13 16:34:19 +0300

    модуль ооп + доработка модуля qa

--- oop/src/main/kotlin/ru/sber/oop/Polymorphism.kt
+++ oop/src/main/kotlin/ru/sber/oop/Polymorphism.kt
@@ -1,13 +1,54 @@
 package ru.sber.oop
 
+import kotlin.random.Random
+
 interface Fightable {
     val powerType: String
     var healthPoints: Int
     val damageRoll: Int
+        get() = Random.nextInt()
 
     fun attack(opponent: Fightable): Int
 }
 
-//TODO: create class Player, Monster, Goblin here...
+class Player(
+    val name: String,
+    val isBlessed: Boolean,
+    override val powerType: String,
+    override var healthPoints: Int
+) : Fightable {
+    override fun attack(opponent: Fightable): Int {
+        val damage = damageRoll * (if (isBlessed) 2 else 1)
+        opponent.healthPoints -= damage
+        return damage
+    }
+
+}
+
+abstract class Monster(
+    open val name: String,
+    open val description: String,
+    override val powerType: String,
+    override var healthPoints: Int
+) : Fightable {
+
+    override fun attack(opponent: Fightable): Int {
+        val damage = damageRoll
+        opponent.healthPoints -= damage
+        return damage
+    }
+}
+
+class Goblin(
+    override val name: String,
+    override val description: String,
+    override val powerType: String,
+    override var healthPoints: Int
+) : Monster(name, description, powerType, healthPoints) {
+    override val damageRoll: Int
+        get() = Random.nextInt() / 2
+}
+
+fun Monster.getSalutation(): String = "I'm the big monster $name!"
 
 
