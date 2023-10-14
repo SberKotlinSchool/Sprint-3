e3804dad8bdad2e379934790e97879f38f1dc0f0
ivan <strateg8888@gmail.com>
2023-10-14 15:50:23 +0400

    IO NIO Homework

--- io/src/main/kotlin/ru/sber/io/Archivator.kt
+++ io/src/main/kotlin/ru/sber/io/Archivator.kt
@@ -1,16 +1,33 @@
 package ru.sber.io
 
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
+import java.util.zip.ZipOutputStream
+
 /**
  * Реализовать методы архивации и разархивации файла.
  * Для реализиации использовать ZipInputStream и ZipOutputStream.
  */
 class Archivator {
+    private val logFile = File("io/logfile.log")
+    private val logFileZip = File("io/logfile.zip")
+    private val unzipLogfile = File("io/unzipLogfile.log")
 
     /**
      * Метод, который архивирует файл logfile.log в архив logfile.zip.
      * Архив должен располагаться в том же каталоге, что и исходной файл.
      */
     fun zipLogfile() {
+        ZipOutputStream(logFileZip.outputStream()).use {
+            output -> FileInputStream(logFile).use {
+                file -> val entry = ZipEntry(logFile.name)
+            output.putNextEntry(entry)
+            file.copyTo(output)
+        }
+        }
 
     }
 
@@ -19,6 +36,12 @@ class Archivator {
      * Извлечь из архива logfile.zip файл и сохарнить его в том же каталоге с именем unzippedLogfile.log
      */
     fun unzipLogfile() {
+        FileOutputStream(unzipLogfile).use {
+            file -> ZipInputStream(logFileZip.inputStream()).use {
+                input -> val entry = input.nextEntry
+            if (entry != null) input.copyTo(file)
+        }
+        }
 
     }
 }
