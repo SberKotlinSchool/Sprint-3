359bf5c27d89345309168ed664356908de014525
maximastashkin <maksim.astash@gmail.com>
2023-10-16 19:10:15 +0300

    Sprint-3 OOP task done

--- oop/src/main/kotlin/ru/sber/oop/DataClasses.kt
+++ oop/src/main/kotlin/ru/sber/oop/DataClasses.kt
@@ -1,11 +1,12 @@
 package ru.sber.oop
 
-data class User(val name: String, val age: Long) {
-    lateinit var city: String
-}
+data class User(val name: String, val age: Long, var city: String? = null)
 
 fun main() {
     val user1 = User("Alex", 13)
-    //TODO: user2 = ...
-    //TODO: user3 = ...
+    val user2 = user1.copy(name = "User2")
+    user1.city = "Omsk"
+    val user3 = user1.copy()
+    user3.city = "Tomsk"
+    println(user1 == user3)
 }
