39b6ae7b5df4b03e0b4355a9f38c343edc944b1b
alexanastasyev <alex.anastasyev@mail.ru>
2023-10-21 14:42:56 +0300

    io/nio task

--- io/src/main/kotlin/ru/sber/Main.kt
+++ io/src/main/kotlin/ru/sber/Main.kt
@@ -1,5 +1,11 @@
 package ru.sber
 
-fun main() {
+import ru.sber.io.Archivator
+import ru.sber.nio.Grep
 
+fun main() {
+//    Archivator().zipLogfile()
+//    Archivator().unzipLogfile()
+
+    Grep().find("GET")
 }
