b2f0e1643d976858d289f9559acc3a0d329a3872
Федор Филиппов <fefilippov@edu.hse.ru>
2023-10-21 10:23:26 +0300

    added a test for processNextRequest

--- qa/src/test/kotlin/ru/sber/qa/HrDepartmentTest.kt
+++ qa/src/test/kotlin/ru/sber/qa/HrDepartmentTest.kt
@@ -1,9 +1,11 @@
 package ru.sber.qa
 
 import io.mockk.*
+import junit.framework.TestCase.assertTrue
 import org.junit.Test
-import java.time.DayOfWeek
-import java.time.LocalDateTime
+import java.lang.reflect.Field
+import java.time.*
+import java.util.*
 import kotlin.test.assertFailsWith
 
 class HrDepartmentTest {
@@ -39,6 +41,34 @@ class HrDepartmentTest {
         every { certificateRequest.certificateType } returns CertificateType.LABOUR_BOOK
 
         assertFailsWith<NotAllowReceiveRequestException>(block = { HrDepartment.receiveRequest(certificateRequest) })
+
+        unmockkStatic(LocalDateTime::class)
+    }
+
+    @Test
+    fun processNextRequestTest() {
+        mockkStatic(LocalDateTime::class)
+
+        val certificateRequest = mockk<CertificateRequest>()
+        val certificate = mockk<Certificate>()
+
+        every { LocalDateTime.now(HrDepartment.clock).dayOfWeek } returns DayOfWeek.MONDAY
+
+        every { certificateRequest.process(any()) } returns certificate
+
+        val incomeBoxField: Field = HrDepartment::class.java.getDeclaredField("incomeBox")
+        incomeBoxField.isAccessible = true
+        val incomeBox = incomeBoxField.get(HrDepartment) as LinkedList<CertificateRequest>
+        incomeBox.push(certificateRequest)
+
+        HrDepartment.processNextRequest(13L)
+
+        verify { certificateRequest.process(13L) }
+
+        val outcomeOutcomeField: Field = HrDepartment::class.java.getDeclaredField("outcomeOutcome")
+        outcomeOutcomeField.isAccessible = true
+        val outcomeOutcome = outcomeOutcomeField.get(HrDepartment) as LinkedList<Certificate>
+        assertTrue(outcomeOutcome.contains(certificate))
 
         unmockkStatic(LocalDateTime::class)
     }
